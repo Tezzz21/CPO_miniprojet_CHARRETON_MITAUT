c1238b219e4a539c381120765521a344f6fbb0d2
Tess Charreton <tess.charreton@epfedu.fr>
2024-11-29 12:28:43 +0100

    Merge origin/master
    Conflicts:
    	Projet_MiniJeu/src/projet_minijeu/Projet_MiniJeu.java

---
+++ Projet_MiniJeu/src/projet_minijeu/Cellule.java
@@ -0,0 +1,53 @@
+package projet_minijeu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tessc
+ */
+public class Cellule {
+    private boolean presenceBombe;
+    private boolean devoilee;
+    int nbBombesAdjacentes;
+    
+    public boolean getPresenceBombe(){
+        return presenceBombe;
+    }
+    public int getNbBombesAdjacentes(){
+        return nbBombesAdjacentes;
+    }
+    public void placerBombe() {
+        this.presenceBombe = true; // Place une bombe dans la cellule
+    }
+    public void revelerCellule() {
+        this.devoilee = true; // Révèle la cellule
+    }
+    public void setNbBombesAdjacentes(int nbBombesAdjacentes ){
+        if (nbBombesAdjacentes>=0){
+            this.nbBombesAdjacentes=nbBombesAdjacentes;
+        }
+        else {
+            nbBombesAdjacentes=-nbBombesAdjacentes;
+        }
+    }
+    @Override
+    public String toString() {
+        if(devoilee==false){
+            return "?";
+        }
+        if (devoilee==true && presenceBombe==true){
+            return "B";
+        }
+        if (devoilee==true && presenceBombe==false && nbBombesAdjacentes>0){
+            return String.valueOf(nbBombesAdjacentes);
+        }
+        if (devoilee==true && presenceBombe==false && nbBombesAdjacentes==0){
+            return " ";
+        }
+        return null;
+    }
+}
--- Projet_MiniJeu/src/projet_minijeu/Projet_MiniJeu.java
+++ Projet_MiniJeu/src/projet_minijeu/Projet_MiniJeu.java
@@ -14,7 +14,8 @@ public class Projet_MiniJeu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
    
         
