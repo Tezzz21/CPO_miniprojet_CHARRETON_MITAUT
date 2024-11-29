fcb94bd8d2cf5bafd89f7378a9de2b36ab030c45
Tess Charreton <tess.charreton@epfedu.fr>
2024-11-29 11:38:38 +0100

    Merge origin/master
    Conflicts:
    	Projet_MiniJeu/src/projet_minijeu/Projet_MiniJeu.java

--- Projet_MiniJeu/src/Cellule.java
+++ Projet_MiniJeu/src/Cellule.java
@@ -18,4 +18,10 @@ public class Cellule {
     public int getNbBombesAdjacentes(){
         return nbBombesAdjacentes;
     }
+    public void placerBombe() {
+        this.presenceBombe = true; // Place une bombe dans la cellule
+    }
+    public void revelerCellule() {
+        this.devoilee = true; // Révèle la cellule
+    }
 }
