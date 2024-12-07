11702e10a78885968be68cddbdb87722514a3bcf
Tess Charreton <tess.charreton@epfedu.fr>
2024-12-07 18:39:40 +0100

    Merge origin/master
    Conflicts:
    	Projet_MiniJeu/src/projet_minijeu/Projet_MiniJeu.java

--- Projet_MiniJeu/src/projet_minijeu/Partie.java
+++ Projet_MiniJeu/src/projet_minijeu/Partie.java
@@ -11,14 +11,26 @@ package projet_minijeu;
 public class Partie {
     private GrilleDeJeu grille;
     private int nbVies;
-    private boolean victoire;
+    private boolean enCours;
     
     // Constructeur
     public Partie(int nbLignes, int nbColonnes, int nbBombes, int nbVies) {
         this.grille = new GrilleDeJeu(nbLignes, nbColonnes,nbBombes);
         this.nbVies = nbVies;
-        this.victoire = false;
+        this.enCours = true;
     }
+    private void initialiserPartie() {
+        grille.placerBombesAleatoirement();
+        this.nbVies = nbVies;           
+        this.enCours = true;
+        }
     
-    
+    public void tourDeJeu(int ligne, int colonne){
+        if (!enCours) {
+            System.out.println("La partie est terminée.");
+            return;
+        }
+        grille.revelerCellule(ligne, colonne);
+        
+    }
 }
